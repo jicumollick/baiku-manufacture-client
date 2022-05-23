9bc2c99cfcb39747b92a4a604717e3398e964a17
jicu mollick <jikumollick82@gmail.com>
2022-05-23 13:59:44 +0600

    Banner Done

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import bike from "../../Assets/images/bike.png";
 const Banner = () => {
   return (
-    <div>
-      <h2>I am Banner</h2>
+    <div style={{ backgroundColor: "lightblue", padding: "20px 40px" }}>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12 col-md-6">
+            <div>
+              <h1>
+                Welcome to <br />
+                <span className="primary-color"> Baiku Manufacture</span>{" "}
+              </h1>
+              <p>
+                <small>Your trusted Bike House</small>
+              </p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6">
+            <div>
+              <img src={bike} className="img-fluid" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
