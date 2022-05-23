000b9289f6367b9cb73be5cb008054789370c8f7
jicu mollick <jikumollick82@gmail.com>
2022-05-23 14:40:20 +0600

    Footer Done

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>I am Footer</h2>
+    <div className="bg-dark text-light">
+      <div class="container">
+        <div class="row">
+          <div class="col py-2">
+            {" "}
+            &copy; 2022, Baiku Manufacture , Dhaka Bangladesh, All right
+            Reserved
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
